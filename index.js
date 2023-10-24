c7fd53fe065656c3f07e654a1364753917c0a80a
Ansari <jaa923285@tatatechnologies.com>
2023-10-24 21:38:18 +0530

    reverse sentence

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.table('Welcome to javascript');
+const text = "My name is jamil";
+
+let words = text.split(" "); //["My name is jamil"]
+let output = "";
+for (let i = 0; i < words.length; i++) {
+  let reverseStr = words[i];
+  for (let j = reverseStr.length - 1; j >= 0; j--) {
+    output += reverseStr[j];
+  }
+  
+  if (i < words.length - 1) {
+    output += " "; // Add a space between words
+  }
+}
+
+console.log(output); // yM eman si limaj
